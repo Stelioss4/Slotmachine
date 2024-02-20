c67c8a5b9979e9802e0e927148364112f5dce8f4
Stelioss4 <stelios.boursanidis@gmail.com>
2024-02-20 11:05:38 +0100

    I changed the way I write the list and used bool to define the row/col matches

--- Program.cs
+++ Program.cs
@@ -1,35 +1,22 @@
 ﻿using System;
-
 namespace Slotmachine
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
             const int GAME_PRICE = 3;
             const char ACE = 'A';
             const char KING = 'K';
             const char QUEEN = 'Q';
 
-            Console.WriteLine("Hello! Lets play the slotmachine!");
+            Console.WriteLine("Hello! Let's play the slot machine!");
             Console.WriteLine("\n*********************************\n");
 
-            Console.WriteLine($"Please insert some money! \nThe game preis is {GAME_PRICE}$");
+            Console.WriteLine($"Please insert some money! \nThe game price is {GAME_PRICE}$");
             double money = Convert.ToDouble(Console.ReadLine());
 
-            char[,] figureList = new char[3, 3];
-            figureList[0, 0] = ACE;
-            figureList[0, 1] = KING;
-            figureList[0, 2] = QUEEN;
-            figureList[1, 0] = ACE;
-            figureList[1, 1] = KING;
-            figureList[1, 2] = QUEEN;
-            figureList[2, 0] = ACE;
-            figureList[2, 1] = KING;
-            figureList[2, 2] = QUEEN;
-
-            Random random = new Random();
+            Random rng = new Random();
 
             while (true)
             {
@@ -40,55 +27,55 @@ namespace Slotmachine
                 if (play == ' ' && money >= GAME_PRICE)
                 {
                     money -= GAME_PRICE;
-                    for (int i = 0; i < figureList.GetLength(0); i++)
+
+                    char[] figures = { ACE, KING, QUEEN };
+                    char[,] figureList = new char[3, 3];
+
+                    for (int lineIndex = 0; lineIndex < 3; lineIndex++)
                     {
-                        for (int j = 0; j < figureList.GetLength(0); j++)
+                        for (int verticalIndex = 0; verticalIndex < 3; verticalIndex++)
                         {
-                            int randomrow = random.Next(0, figureList.GetLength(1));
-                            char randomFigure = figureList[i, randomrow];
-                            Console.Write(randomFigure);
+                            int randomIndex = rng.Next(0, 3);
+                            figureList[lineIndex, verticalIndex] = figures[randomIndex];
+                            Console.Write(figureList[lineIndex, verticalIndex]);
                         }
                         Console.WriteLine();
-
-
-
                     }
-                   
-                    if (money < GAME_PRICE)
+
+                    for (int lineIndex = 0; lineIndex < 3; lineIndex++)
                     {
-                        Console.WriteLine("Please insert more money!");
-                        money = Convert.ToDouble(Console.ReadLine());
-                        continue;
-                    }
-                    for (int lineindex = 0; lineindex < figureList.GetLength(0); lineindex++)
-                    {
-                        if (figureList[lineindex, 0] == figureList[lineindex, 1] && figureList[lineindex, 1] == figureList[lineindex, 2])
+                        bool rowMatch = figureList[lineIndex, 0] == figureList[lineIndex, 1] && figureList[lineIndex, 1] == figureList[lineIndex, 2];
+                        if (rowMatch)
                         {
-                            Console.WriteLine("Congratulations! You've got a match in horizontal line!");
+                            Console.WriteLine("Congratulations! You've got a Horizontal match!");
                             money += GAME_PRICE;
+                            break;
                         }
-
                     }
-                    for (int verticalindex = 0; verticalindex < figureList.GetLength(0);  verticalindex++)
+                    for (int verticalIndex = 0; verticalIndex < 3; verticalIndex++)
                     {
-                        if (figureList[0, verticalindex] == figureList[1, verticalindex] && figureList[1, verticalindex] == figureList[2, verticalindex])
+                        bool colMatch = figureList[0, verticalIndex] == figureList[1, verticalIndex] && figureList[1, verticalIndex] == figureList[2, verticalIndex];
+                        if (colMatch)
                         {
-                            Console.WriteLine("Congratulations! You've got a match in horizontal line!");
+                            Console.WriteLine("Congratulations! You've got a Vertical match!");
                             money += GAME_PRICE;
+                            break;
                         }
-
                     }
                 }
                 else
                 {
-                    Console.WriteLine("\nOK! Goodbuy!");
+                    Console.WriteLine("\nOK! Goodbye!");
                     break;
+                }
+
+                if (money < GAME_PRICE)
+                {
+                    Console.WriteLine("Please insert more money!");
+                    money = Convert.ToDouble(Console.ReadLine());
                 }
                 Console.WriteLine($"Your remaining money is {money}$");
             }
         }
     }
 }
-
-
-
